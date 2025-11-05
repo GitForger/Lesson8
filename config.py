377824ae8298590b2ede0f55c98c5518e969c4c6
GitForger <trueforgefather@gmail.com>
2025-11-06 01:26:14 +0700

    PEP8 config.py

--- config.py
+++ config.py
@@ -1,19 +1,23 @@
 import requests
 BASE_URL = "https://ru.yougile.com/api-v2"
 
+
 YOUGILE_LOGIN = "имейл@имейл.com"
 YOUGILE_PASSWORD = "пароль"
+
 
 auth_response = requests.post(
     f"{BASE_URL}/auth/login",
     json={"login": YOUGILE_LOGIN, "password": YOUGILE_PASSWORD}
 )
 
+
 if auth_response.status_code == 200:
     TOKEN = auth_response.json()["access_token"]
 else:
     print(f"Auth failed: {auth_response.status_code}")
     TOKEN = None
+
 
 HEADERS = {
     "Content-Type": "application/json",
